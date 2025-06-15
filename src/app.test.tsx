7d0cf78def66dea59b1b7682ec5d7fb2ddce3ab4
Henrique Albuquerque <henriquealbuquerqueb6@gmail.com>
2025-06-15 11:54:52 -0300

    fix: tests setup

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,6 +1,20 @@
-import { afterEach, describe, it } from "vitest";
+import { afterEach, describe, it, vi } from "vitest";
 import { cleanup, render } from "@testing-library/react";
 import App from "./app";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
 
 afterEach(() => {
   cleanup();
